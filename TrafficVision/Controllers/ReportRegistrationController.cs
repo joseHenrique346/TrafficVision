3875117a541a1f273e8a95612f7f3ae3db6cc501
CarlosLeutcke <Ch772707@gmail.com>
2025-11-17 16:09:30 -0300

    Criei as controllers de User e ReportRegistration

---
+++ TrafficVision/Controllers/ReportRegistrationController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrafficVision.Domain.Entities;
+using TrafficVision.Domain.Interfaces.Repository;
+
+namespace TrafficVision.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportRegistrationController : ControllerBase
+    {
+        private readonly IReadReportRegistrationRepository _readRepository;
+        private readonly IWriteReportRegistrationRepository _writeRepository;
+
+        public ReportRegistrationController(
+            IReadReportRegistrationRepository readRepository,
+            IWriteReportRegistrationRepository writeRepository)
+        {
+            _readRepository = readRepository;
+            _writeRepository = writeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ReportRegistration>>> GetAllAsync()
+        {
+            var list = await _readRepository.GetAll();
+            return Ok(list);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ReportRegistration>> GetByIdAsync(long id)
+        {
+            var result = await _readRepository.GetByIdAsync(id);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateAsync([FromBody] ReportRegistration entity)
+        {
+            await _writeRepository.AddAsync(entity);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateAsync([FromBody] ReportRegistration entity)
+        {
+            await _writeRepository.UpdateAsync(entity);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAsync(long id)
+        {
+            await _writeRepository.DeleteAsync(id);
+            return Ok();
+        }
+    }
+}
---
+++ TrafficVision/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrafficVision.Domain.Entities;
+using TrafficVision.Domain.Interfaces.Repository;
+
+namespace TrafficVision.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IReadUserRepository _readRepository;
+        private readonly IWriteUserRepository _writeRepository;
+
+        public UserController(IReadUserRepository readRepository, IWriteUserRepository writeRepository)
+        {
+            _readRepository = readRepository;
+            _writeRepository = writeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetAllAsync()
+        {
+            var users = await _readRepository.GetAll();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetByIdAsync(long id)
+        {
+            var user = await _readRepository.GetByIdAsync(id);
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateAsync([FromBody] User user)
+        {
+            await _writeRepository.AddAsync(user);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateAsync([FromBody] User user)
+        {
+            await _writeRepository.UpdateAsync(user);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAsync(long id)
+        {
+            await _writeRepository.DeleteAsync(id);
+            return Ok();
+        }
+    }
+}
